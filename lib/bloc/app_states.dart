a7b2624fc3fed10234c0c721309988f870637bd6
p-jura <themadman@o2.pl>
2023-03-31 08:53:50 +0200

    adds bloc app states

--- lib/bloc/app_states.dart
+++ lib/bloc/app_states.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart' show immutable;
 import 'package:online_photo_libery/auth/auth_error.dart';
 
@@ -13,9 +15,72 @@ abstract class AppState {
 }
 
 @immutable
-class AppStatLoggedIn extends AppState {
-  const AppStatLoggedIn({
+class AppStateLoggedIn extends AppState {
+  final User user;
+  final Iterable<Reference> images;
+
+  const AppStateLoggedIn({
+    required this.user,
+    required this.images,
     required super.isLoading,
     required super.authError,
   });
+  @override
+  bool operator ==(other) {
+    final otherClass = other;
+    if (otherClass is AppStateLoggedIn) {
+      return isLoading == otherClass.isLoading &&
+          user == otherClass.user &&
+          images.length == otherClass.images.length;
+    } else {
+      return false;
+    }
+  }
+
+  @override
+  int get hashCode => Object.hash(
+        user.uid,
+        images,
+      );
+  @override
+  String toString() => 'AppStateLoggedIn, images.lenght = ${images.length}';
+}
+
+@immutable
+class AppStateLogOut extends AppState {
+  const AppStateLogOut({
+    required super.isLoading,
+    required super.authError,
+  });
+  @override
+  String toString() =>
+      'AppStateLoggedOut, isLoading = $isLoading, authError = $authError';
+}
+
+@immutable
+class AppStateIsInRegistrationView extends AppState {
+  const AppStateIsInRegistrationView(
+      {required super.isLoading, required super.authError});
+}
+
+extension GetUser on AppState {
+  User? get user {
+    final presentState = this;
+    if (presentState is AppStateLoggedIn) {
+      return presentState.user;
+    } else {
+      return null;
+    }
+  }
+}
+
+extension GetImages on AppState {
+  Iterable<Reference>? get user {
+    final presentState = this;
+    if (presentState is AppStateLoggedIn) {
+      return presentState.images;
+    } else {
+      return null;
+    }
+  }
 }
