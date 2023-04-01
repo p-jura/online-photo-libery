341b698b1699df779f7f4cf5f3a93dd1c0bc1570
p-jura <themadman@o2.pl>
2023-04-01 13:49:35 +0200

    adds login_view

--- lib/extension/if_debugging.dart
+++ lib/extension/if_debugging.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/foundation.dart' show kDebugMode;
 
 extension IfDebugging on String {
-  String? get ifDebuggin => kDebugMode ? this : null;
+  String? get ifDebugging => kDebugMode ? this : null;
 }
